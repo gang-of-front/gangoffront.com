c1a54bd1e7f725a10408572fcfb827d6da86c6cd
Rafael Antonio Lucio <rafaelantoniolucio@gmail.com>
2022-11-22 14:53:32 -0300

    chore(functions): fix unused variable

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -2,7 +2,7 @@ export function onRequest({ request, next }) {
   const url = new URL(request.url);
 
   if (url.pathname.startsWith("proxy")) {
-    return new Response(JSON.stringify(context, undefined, 2));
+    return new Response(JSON.stringify(url, undefined, 2));
   }
 
   return next();
