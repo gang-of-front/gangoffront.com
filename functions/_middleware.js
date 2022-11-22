dac2c69d0fb94b3961da332b36a23567bb9e4b2e
Rafael Antonio Lucio <rafaelantoniolucio@gmail.com>
2022-11-22 14:36:54 -0300

    chore(functions): update pages functions

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,18 +1,3 @@
-addEventListener("fetch", (event) => {
-  event.respondWith(
-    handleRequest(event.request).catch(
-      (err) => new Response(err.stack, { status: 500 })
-    )
-  );
-});
-
-async function handleRequest({ request, next }) {
-  const res = await next();
-  const { url } = new URL(request.url);
-
-  if (url.pathname.startsWith("/realworld")) {
-    return fetch(`https://realworld-qwik.gangoffront.com?`);
-  }
-  
-  return res;
+export function onRequest(context) {
+  return new Response("Hello, world!")
 }
