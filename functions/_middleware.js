e057ee6b50d0b1405b7de4958b54ef7ef7cfcfc6
Rafael Antonio Lucio <rafaelantoniolucio@gmail.com>
2022-11-22 15:27:06 -0300

    chore(functions): update functions setup

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -2,7 +2,7 @@ export function onRequest({ request, next }) {
   const url = new URL(request.url);
 
   if (url.pathname === "proxy") {
-    return new Response(JSON.stringify(url, undefined, 2));
+    return new Response(JSON.stringify(request, undefined, 2));
   } else {
     return next();
   }
