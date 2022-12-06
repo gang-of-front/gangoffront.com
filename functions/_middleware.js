71a546b1e98c0e13eb872cd987a6de5174bee224
Guilherme Mangabeira Gregio <guilherme.gregio@creditas.com.br>
2022-12-06 18:31:13 -0300

    chore: test middleware

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,9 +1,4 @@
-export function onRequest({ request, next }) {
-  const url = new URL(request.url);
-
-  if (url.pathname === "proxy") {
-    return new Response(JSON.stringify(request, undefined, 2));
-  } else {
-    return next();
-  }
+export function onRequest(context) {
+  console.log({ context })
+  return new Response('hello world');
 }
