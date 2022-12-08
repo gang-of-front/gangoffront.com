ea87417671f8c0678938401d551a0035ff964e44
Guilherme Mangabeira Gregio <guilherme.gregio@creditas.com.br>
2022-12-08 20:18:56 -0300

    chore: proxy console

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -4,8 +4,8 @@ export async function onRequest(context) {
   const url = new URL(context.request.url)
 
   if (url.pathname.startsWith('/test')) {
-    console.log('proxy', {context, url})
-    return new Repsonse('proxy response')
+    console.log('proxy', { context, url })
+    return new Response('proxy response')
   }
 
   return new Response('hello world');
