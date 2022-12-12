852a8d9cfd8ebdcd48d6555d959443f4d4e9f0ea
Guilherme Mangabeira Gregio <guilherme.gregio@creditas.com.br>
2022-12-12 19:42:45 -0300

    refactor: use middleware native

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,21 +1,6 @@
-async function seq(...fns) {
-  let exec = true
-  let result = null
+const proxy = (regexp,destination, fn) => async (context) => {
+  const url = new URL(context.request.url)
 
-
-  while (exec) {
-    const fn = fns.shift()
-    result = await fn?.()
-
-    if (result || fn === undefined) {
-      exec = false
-    }
-  }
-
-  return result
-}
-
-const proxy = (url, regexp, destination, fn) => async () => {
   const matchResult = url.pathname.match(regexp)
 
   if (matchResult && matchResult[1]) {
@@ -30,32 +15,17 @@ const proxy = (url, regexp, destination, fn) => async () => {
     return response
   }
 
-  return null
+  return context.next()
 }
 
-export async function onRequest({ request, next }) {
-  const url = new URL(request.url)
-
-  const response = await seq(
-    proxy(url, /\/root-mf-logged-area\/(.*)/, 'https://root-mf-logged-area-staging.netlify.app/root-mf-logged-area/:splat'),
-    proxy(url, /\/auth\/(.*)/, 'https://root-mf-logged-area-staging.netlify.app/auth/:splat'),
-    proxy(url, /(\/import-map.json)/, 'https://growth-import-map-logged-area-staging.s3.amazonaws.com/import-map.json'),
-    proxy(url, /(\/import-map2.json)/, 'https://growth-import-map-logged-area-staging.s3.amazonaws.com/import-map.json', config => {
-      config.headers.set('Access-Control-Allow-Origin', '*')
-      config.headers.set('X-Frame-Options', 'DENY')
-      config.headers.set('X-Content-Type-Options', 'nosniff')
-    }),
-    proxy(url, /(\/import-map3.json)/, 'https://growth-import-map-logged-area-staging.s3.amazonaws.com/import-map.json', config => {
-      config.headers.set('Access-Control-Allow-Origin', '*')
-      config.headers.set('X-Frame-Options', 'DENY')
-      config.headers.set('X-Content-Type-Options', 'nosniff')
-      config.headers.set('Cache-Control', 'age=1500')
-    }),
-  )
-
-  if (response) {
-    return response
-  }
-
-  return next()
-}
+export const onRequest = [
+  proxy(/(\/import-map.json)/, 'https://growth-import-map-logged-area-staging.s3.amazonaws.com/import-map.json'),
+  proxy(/\/root-mf-logged-area\/(.*)/, 'https://root-mf-logged-area-staging.netlify.app/root-mf-logged-area/:splat'),
+  proxy(/\/auth\/(.*)/, 'https://root-mf-logged-area-staging.netlify.app/auth/:splat'),
+  proxy(/(\/import-map2.json)/, 'https://growth-import-map-logged-area-staging.s3.amazonaws.com/import-map.json', (response)=>{
+    response.headers.set('Access-Control-Allow-Origin', '*')
+    response.headers.set('X-Frame-Options', 'DENY')
+    response.headers.set('X-Content-Type-Options', 'nosniff')
+    response.headers.set('Cache-Control', 'age=1500')
+  }),
+];
