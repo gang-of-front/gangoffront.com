db6f237cb7c6d5b07a6dafe3ca98a465d13b4776
Guilherme Mangabeira Gregio <guilherme.gregio@creditas.com.br>
2022-12-12 16:19:02 -0300

    chore: fetch other url

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -8,12 +8,25 @@ export async function onRequest({ request, next, env }) {
   if (url.pathname === "/import-map2.json") {
     const head = await fetch('https://growth-import-map-logged-area-staging.s3.amazonaws.com/import-map.json')
 
-    console.log({ head })
-    return new Response(head.json(), {
-      'Access-Control-Allow-Origin': '*',
-      'X-Frame-Options': "DENY",
-      'X-Content-Type-Options': "nosniff"
-    })
+    const response = new Response(JSON.stringify(head.json()))
+
+    response.headers.set('Access-Control-Allow-Origin', '*')
+    response.headers.set('X-Frame-Options', 'DENY')
+    response.headers.set('X-Content-Type-Options', 'nosniff')
+
+    return response
+  }
+
+  if (url.pathname === "/import-map3.json") {
+    const head = await fetch('https://growth-import-map-logged-area-staging.s3.amazonaws.com/import-map.json')
+
+    const response = new Response(JSON.stringify(head.json()), head)
+
+    response.headers.set('Access-Control-Allow-Origin', '*')
+    response.headers.set('X-Frame-Options', 'DENY')
+    response.headers.set('X-Content-Type-Options', 'nosniff')
+
+    return response
   }
 
   return new Response('hello world');
