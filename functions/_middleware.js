befd21009acfb615ee839661cbdb517ccd9b630f
Guilherme Mangabeira Gregio <guilherme.gregio@creditas.com.br>
2022-12-12 16:27:51 -0300

    chore: fetch other url

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -29,5 +29,18 @@ export async function onRequest({ request, next, env }) {
     return response
   }
 
+  if (url.pathname === "/import-map4.json") {
+    const head = await fetch('https://growth-import-map-logged-area-staging.s3.amazonaws.com/import-map.json')
+
+    const response = new Response(JSON.stringify(await head.json()), head)
+
+    response.headers.set('Access-Control-Allow-Origin', '*')
+    response.headers.set('X-Frame-Options', 'DENY')
+    response.headers.set('X-Content-Type-Options', 'nosniff')
+    response.headers.set('Cache-Control', 'max-age=1500')
+
+    return response
+  }
+
   return new Response('hello world');
 }
